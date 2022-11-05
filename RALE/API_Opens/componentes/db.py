fb1da0c648e870246dc56f3aec725e7c80057f1f
Ubuntu <ubuntu@ip-172-31-22-175.ec2.internal>
2022-11-05 02:53:31 +0000

    Utilizacion de Api Gmail, leer correos, clasificarlos, etiquetarlos, crear borrador, guardar en base de datos

--- RALE/API_Opens/componentes/db.py
+++ RALE/API_Opens/componentes/db.py
@@ -7,28 +7,7 @@ def migracion_datos(datos):
 	insert = str(datos[0]),str(datos[1]),str(datos[2]),str(datos[3])
 	cursor.execute(sql,insert)
 	conexion.commit()
-
-def consulta_cuerpo():
-	conexion = psycopg2.connect(database = "Opens", user = "opensrale", password = "rale2022", host = "localhost", port = "5432")
-	cursor = conexion.cursor()
-	sql = 'SELECT cuerpo FROM "Email"'
-	cursor.execute(sql)
-	data_cuerpo = []
-	for sentencia in cursor:
-		data_cuerpo.extend(sentencia)
 	conexion.close()
-	return data_cuerpo
-
-def consulta_destinatario():
-	conexion = psycopg2.connect(database = "Opens", user = "opensrale", password = "rale2022", host = "localhost", port = "5432")
-	cursor = conexion.cursor()
-	sql = 'SELECT nombre FROM "Email"'
-	cursor.execute(sql)
-	data_from = []
-	for sentencia in cursor:
-		data_from.extend(sentencia)
-	conexion.close()
-	return data_from
 
 def consulta_plantilla():
         conexion = psycopg2.connect(database = "Opens", user = "opensrale", password = "rale2022", host = "localhost", port = "5432")
--- RALE/API_Opens/componentes/gmail.py
+++ RALE/API_Opens/componentes/gmail.py
@@ -4,10 +4,10 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import base64
 from email.message import EmailMessage
-
-# If modifying these scopes, delete the file token.json.
+from bs4 import BeautifulSoup
+from db import migracion_datos, consulta_plantilla
+import base64, joblib, email, sys
 
 def create_service():
     ruta = sys.path
@@ -31,15 +31,101 @@ def create_service():
 
     return service
 
-def crear_borrador(cuerpo,cliente,asunto):
+def crear_etiqueta():
     service = create_service()
-    body = cuerpo
+    label_body = {"name": "Aplica","messageListVisibility": "show","labelListVisibility": "labelShow",}
+    service.users().labels().create(userId='me', body=label_body).execute()
+    label_body = {"name": "No Aplica","messageListVisibility": "show","labelListVisibility": "labelShow",}
+    service.users().labels().create(userId='me', body=label_body).execute()
+
+def mostrar_etiqueta():
+    service = create_service()
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    etiquetas = []
+    for label in labels:
+        nombre = label['id']
+        etiquetas.append(nombre)
+    if len(etiquetas) == 14:
+        crear_etiqueta()
+    else:
+        pass
+
+def crear_borrador(cuerpo,cliente,asunto,solicitud):
+    service = create_service()
+    body = "En relación al asunto:\n\n<"+str(solicitud)+">\n\n"+cuerpo
     message = EmailMessage()
     message['from'] = 'portafolio.duocuc.2022@gmail.com'
-    message['to'] = cliente
-    message['subject'] = asunto
+    message['to'] = str(cliente)
+    message['subject'] = str(asunto)
     message.set_content(body)
     encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
     create_message = {'message': {'raw': encoded_message}}
     drafts =  service.users().drafts().create(userId='me',body=create_message).execute()
 
+def leer_email():
+    service = create_service()
+    resultado = service.users().messages().list(userId = 'me', labelIds = ['INBOX'], q = 'is:unread', maxResults = 2).execute()
+    emails = resultado.get('messages',[])
+    data_email = []
+    for msg in emails:
+        temp = []
+        rst = service.users().messages().get(userId='me', id=msg['id']).execute()
+        payload = rst['payload']['headers']
+        for value in payload:
+            if value['name'] == 'From':
+                name = value['value']
+                temp.append(name)
+            if value['name'] == 'Date':
+                fecha = value['value']
+                temp.append(fecha)
+            if value['name'] == 'Subject':
+                asunto = value['value']
+                temp.append(asunto)
+        payload = rst['payload']
+        parts = payload.get('parts')[0]
+        data = parts['body']['data']
+        data = data.replace("-","+").replace("_","/")
+        decoded_data = base64.b64decode (bytes(data, 'UTF-8'))
+        soup = BeautifulSoup(decoded_data , "lxml")
+        mssg_body = soup.body()
+        temp.append(mssg_body)
+        temp.append(rst['id'])
+        data_email.append(temp)
+
+    return data_email
+
+def clasificar():
+    ruta = sys.path
+    email = leer_email()
+    valor = []
+    for cuerpo in range(len(email)):
+        temp = email[cuerpo]
+        vector = joblib.load(ruta[0]+'/vectorizer.joblib')
+        clasificador = joblib.load(ruta[0]+'/SVC.joblib')
+        data = vector.transform([str(temp[3])])
+        resultado = clasificador.predict(data)
+        temp.append(resultado)
+        valor.append(temp)
+    return valor
+
+def main():
+    emails = clasificar()
+    service = create_service()
+    plantilla = consulta_plantilla()
+    for i in range(len(emails)):
+        temp = emails[i]
+        cliente = temp[0]
+        asunto = plantilla[1]
+        cuerpo = plantilla[2]
+        solicitud = temp[2]
+        if temp[5] == 'Despido':
+            mostrar_etiqueta()
+            crear_borrador(cuerpo = cuerpo, cliente = cliente, asunto = asunto, solicitud = solicitud)
+            service.users().messages().modify(userId='me', id=temp[4] , body={ 'removeLabelIds': ['UNREAD']}).execute()
+            service.users().messages().modify(userId='me', id=temp[4] , body={ 'addLabelIds': ['Label_11']}).execute()
+        else:
+            service.users().messages().modify(userId='me', id=temp[4] , body={ 'removeLabelIds': ['UNREAD']}).execute()
+            service.users().messages().modify(userId='me', id=temp[4] , body={ 'addLabelIds': ['Label_12']}).execute()
+        migracion_datos(datos = temp)
